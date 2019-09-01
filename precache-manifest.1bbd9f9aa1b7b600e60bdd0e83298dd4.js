15bee45b9b4e72ef8222de2e52cb00050642c0f2
David Hürter <d.huerter@me.com>
2019-09-01 14:39:23 +0200

    Updates

---
+++ precache-manifest.1bbd9f9aa1b7b600e60bdd0e83298dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df19a465cd5ce981619ab6a3e208608",
+    "url": "/hazelnuzz-website/index.html"
+  },
+  {
+    "revision": "5d8530291109e8b38e37",
+    "url": "/hazelnuzz-website/static/css/main.103f857c.chunk.css"
+  },
+  {
+    "revision": "856281628acf98f4f10c",
+    "url": "/hazelnuzz-website/static/js/2.93bfb7ad.chunk.js"
+  },
+  {
+    "revision": "5d8530291109e8b38e37",
+    "url": "/hazelnuzz-website/static/js/main.b42e1516.chunk.js"
+  },
+  {
+    "revision": "2ef44129e8fdeae8b4bf",
+    "url": "/hazelnuzz-website/static/js/runtime~main.f57c2e4a.js"
+  },
+  {
+    "revision": "2fc6b27ad00a105d760c097ef6facd55",
+    "url": "/hazelnuzz-website/static/media/profil.2fc6b27a.png"
+  }
+]);
